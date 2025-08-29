b4d948c8beda5bb209f7fa625e875b023754f431
afigueroa94 <a.figueroa9494@gmail.com>
2025-08-28 20:31:59 -0500

    adding header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://m.media-amazon.com/images/I/61nuuPxUvaL.png"} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>My weather app</h1>
         </p>
-        <p>
-      <h1>This is a Test</h1>
-        </p>
+        <p>{Header}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://m.media-amazon.com/images/I/61nuuPxUvaL.png"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get weather
         </a>
       </header>
     </div>
